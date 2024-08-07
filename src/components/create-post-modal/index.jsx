308ea3f95eff670a56b5877682482cfcd6fe2bed
Donald <donald.k@appwrk.com>
2024-08-07 17:24:49 +0530

    Added code for by drag change the order of posts and also resolved some bugs

--- src/components/create-post-modal/index.jsx
+++ src/components/create-post-modal/index.jsx
@@ -118,7 +118,7 @@ const CreatePostModal = ({
   setVideoDurations,
 }) => {
   const isDuplicating = useMemo(
-    () => isEdit === "Published" || isEdit === "SaveAsDraft" || false,
+    () => isEdit === "Published" || false,
     [isEdit]
   );
 

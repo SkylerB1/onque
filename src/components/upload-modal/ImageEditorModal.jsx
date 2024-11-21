14d9e28ca8b4a6526a5c5d2636c5511600228e2b
Old-APPWRK <you@example.com>
2024-11-21 16:35:37 +0530

    Image crop issue solve

--- src/components/upload-modal/ImageEditorModal.jsx
+++ src/components/upload-modal/ImageEditorModal.jsx
@@ -12,6 +12,7 @@ import {
 } from "@material-tailwind/react";
 import { getSource } from "../../utils";
 import ToasterCustomConatiner from "../ToasterCustomConatiner";
+import Compressor from 'compressorjs';
 
 function ImgEditorModal({ show, files, setFiles, toggleModal, index }) {
   const src = getSource(files[index]);
@@ -19,9 +20,36 @@ function ImgEditorModal({ show, files, setFiles, toggleModal, index }) {
 
   const handleFile = async () => {
     const res = await editorRef.current.editor.processImage();
-    const prevFiles = [...files];
-    prevFiles[index] = res?.dest;
-    setFiles(prevFiles);
+    console.log(res, "erdtfgyhujikol")
+    // const prevFiles = [...files];
+    // prevFiles[index] = res?.dest;
+    // setFiles(prevFiles);
+    // toggleModal();
+    if (res?.dest) {
+      const imageBlob = res.dest;
+      new Compressor(imageBlob, {
+        quality: 0.8,
+        maxHeight: 2000,
+        success(compressedBlob) {
+          if (compressedBlob.size <= MAX_SIZE) {
+            const compressedFile = new File([compressedBlob], "compressed-image.jpg", { type: compressedBlob.type });
+
+            const updatedFiles = [...files];
+            updatedFiles[index] = compressedFile;
+            setFiles(updatedFiles);
+
+            console.log('Compressed Image Size:', compressedBlob.size);
+          } else {
+            alert("The image is still too large after compression. Please try a smaller image.");
+          }
+        },
+        error(err) {
+          console.error('Compression failed:', err);
+          alert("Error compressing the image.");
+        }
+      });
+    }
+
     toggleModal();
   };
 
